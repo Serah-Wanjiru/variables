f2737826c36973ffb9dad1ff0dccd80d16e3aee0
Serah Wanjiru <WanjiruMburuuu@gmail.com>
2023-02-02 08:15:14 +0300

    added variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>){
+    var  fullname = "serah"
+    var age = 20
+    var  phonenumber = "0726214035"
+    var weightinKg = 54
+    var isKenyan: Boolean =true
+    var isnotKenyan: Boolean =false
+
+    println(isKenyan)
+
+}
+
